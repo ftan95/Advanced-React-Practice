506f6943881d0e3b1daba861d10bcd4554152118
Frederick Tan <ftan2@scu.edu>
2022-06-22 16:00:30 -0700

    copied over starter code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import CounterClass from './components/Counter/CounterClass'
+import CounterFn from './components/Counter/CounterFn';
+import CounterNumber from './components/Counter/CounterNumber';
+import BuyStockClass from './components/BuyStock/BuyStockClass'
+import { useContext, useState } from 'react';
+import BuyStockFn from './components/BuyStock/BuyStockFn';
+import { CounterContext } from './context/counterContext';
+
+
+const pageInfo = [
+  {
+    pageName: "CounterClass",
+    pageComponent: CounterClass
+  },
+  {
+    pageName: "CounterFn",
+    pageComponent: CounterFn
+  },
+  {
+    pageName: "BuyStockClass",
+    pageComponent: BuyStockClass
+  },
+  {
+    pageName: "BuyStockFn",
+    pageComponent: BuyStockFn
+  }
+]
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState(pageInfo[1])
+  const value = useContext(CounterContext);
+
+  console.log("APP", value)
+  const hanldeChangePage = (activePageInfo) => {
+    setCurrentPage(activePageInfo)
+  }
+
+  const renderCurrentPage = () => {
+    const CurPage = currentPage.pageComponent
+    return <CurPage patrick="patrick" />
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" hello="hello">
+      <header className="App__header">
+        <nav className="App__header__nav">
+          {pageInfo.map(page => {
+            switch (page.pageName) {
+              case "CounterClass":
+                return <a onClick={() => hanldeChangePage(page)} >{page.pageName} <CounterNumber />  </a>
+              default:
+                return <a onClick={() => hanldeChangePage(page)} >{page.pageName}</a>
+            }
+          })}
+        </nav>
       </header>
+      {
+        renderCurrentPage()
+      }
     </div>
   );
 }
---
+++ src/components/Counter/CounterClass.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { withCounter } from "../../hoc/withCounter"
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "CounterClass"
+        }
+        this.hanldeAlert = this.hanldeAlert.bind(this);
+    }
+
+    hanldeAlert() {
+        setTimeout(() => {
+            alert(this.props.counter)
+        }, 5000)
+    }
+
+    render() {
+        console.log('render class compoennt')
+        console.log("props counter class", this.props)
+        return <section>
+            <header>{this.state.title}:{this.props.counter}</header>
+            <button onClick={this.props.handleAdd} >+</button><button onClick={this.props.handleSub}>-</button>
+            <button onClick={this.hanldeAlert}>Alert after 5 s</button>
+        </section>;
+    }
+
+}
+
+
+const CounterContainer = withCounter(Counter);
+
+export default CounterContainer;
